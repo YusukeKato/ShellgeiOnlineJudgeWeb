6c59a39e861ec28c66c4fc77d73d2488be6a6597
YusukeKato <takenoko2tomato2+github@gmail.com>
2024-10-16 09:58:38 +0900

    update button width

--- scripts/generate_index_html.py
+++ scripts/generate_index_html.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 update_date = "2024/10/16"
-version_str = "?version=1053"
+version_str = "?version=1054"
 
 site_title = "シェル芸オンラインジャッジ / SHELLGEI ONLINE JUDGE"
 summary_url = "https://yusukekato.jp/images/summary.jpg"
