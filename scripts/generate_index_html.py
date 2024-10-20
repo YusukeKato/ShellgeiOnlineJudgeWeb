98074019415e4ef3f919ea76099f4acd8866be71
YusukeKato <takenoko2tomato2+github@gmail.com>
2024-10-20 13:50:56 +0900

    CSS修正

--- scripts/generate_index_html.py
+++ scripts/generate_index_html.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 update_date = "2024/10/20"
-version_str = "?version=1061"
+version_str = "?version=1062"
 
 site_title = "シェル芸オンラインジャッジ / SHELLGEI ONLINE JUDGE"
 summary_url = "https://yusukekato.jp/images/summary.jpg"
